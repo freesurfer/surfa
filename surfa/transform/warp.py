395713f31fbca182c66fcc09ad7673d4d3a476f2
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2024-02-17 14:16:44 -0500

    Derive Warp from FramedImage to inherit functionality.

--- surfa/transform/warp.py
+++ surfa/transform/warp.py
@@ -3,10 +3,12 @@ import warnings
 import numpy as np
 
 import surfa as sf
+from surfa import transform
+from surfa.image import FramedImage
 from surfa.image.interp import interpolate
 
 
-class Warp:
+class Warp(FramedImage):
 
     class Format:
         """
@@ -26,12 +28,8 @@ class Warp:
 
     #
     # constructor
-    def __init__(self, data=None, source=None, target=None,
-		 spacing=1, exp_k=0.0, format=Format.abs_crs):
+    def __init__(self, data, source=None, target=None, format=Format.abs_crs, **kwargs):
         """
-        Class constructor. 
-        When it is invoked without any parameters, load(mgzwarp) call is needed after the object is created.
-
         Class variables:
           _data:  deformation field, 4D numpy array (c, r, s, 3)
                The _data (width x height x depth x nframes) is indexed by atlas CRS.
@@ -44,8 +42,6 @@ class Warp:
           _format:         Warp.Format
           _source:         ImageGeometry, source image
           _target:         ImageGeometry, target image
-          _spacing:        int    (this is from m3z, not sure if it is really needed)
-          _exp_k:          double (this is from m3z, not sure if it is really needed)
 
         Parameters
         ----------
@@ -57,21 +53,13 @@ class Warp:
             target geometry
         format : Format
             deformation field format
-        spacing : int
-        exp_k : double
+        **kwargs
+            Extra arguments provided to the FramedImage superclass.
         """
-
-        if (data is None and source is None and target is None):
-            return
-        elif (data is not None and source is not None and target is not None):
-            self._data = data
-            self._format = format
-            self._source  = source
-            self._target  = target
-            self._spacing    = spacing
-            self._exp_k      = exp_k
-        else:
-            raise ValueError('Warp constructor: input parameters error')
+        self.format = format
+        self.source = source
+        basedim = len(data.shape) - 1
+        super().__init__(basedim, data, geometry=target, **kwargs)
 
 
     #
@@ -82,10 +70,26 @@ class Warp:
         """
         return self.transform(*args, **kwargs)
 
-    
+    def new(self, data, source=None, target=None, format=None):
+        """
+        Return a new instance of the warp with updated data. Geometries and format are
+        preserved unless specified.
+        """
+        if source is None:
+            source = self.source
+
+        if target is None:
+            target = self.target
+
+        if format is None:
+            format = self.format
+
+        return self.__class__(data, source, target, metadata=self.metadata)
+
     #
     # Read input mgz warp file
-    def load(self, filename):
+    @staticmethod
+    def load(filename):
         """
         Read input mgz warp file, set up deformation field, source/target geometry
 
@@ -94,34 +98,23 @@ class Warp:
         filename : string
             input mgz warp file
         """
-        
-        mgzwarp = sf.load_volume(filename)
+        warp = sf.load_volume(filename)
 
-        # check if mgzwarp is a volume
-        if (not isinstance(mgzwarp, sf.image.framed.Volume)):
-            raise ValueError('Warp::load() - input is not a Volume')
-        
-        # check if input is a mgzwarp (intent FramedArrayIntents.warpmap)
-        if (mgzwarp.metadata['intent'] != sf.core.framed.FramedArrayIntents.warpmap):
-            raise ValueError('Warp::load() - input is not a mgzwarp Volume')
+        if not isinstance(warp, sf.image.framed.Volume):
+            raise ValueError('input is not a Volume')
 
-        self._data = mgzwarp.data
-        self._format = mgzwarp.metadata['warpfield_dtfmt']
+        if warp.metadata['intent'] != sf.core.framed.FramedArrayIntents.warpmap:
+            raise ValueError('input is not a warp Volume')
 
-        # create ImageGeometry object self._source from mgzwarp.metadata['gcamorph_volgeom_src']
-        self._source = sf.transform.geometry.volgeom_dict2image_geometry(mgzwarp.metadata['gcamorph_volgeom_src'])
-
-        # create ImageGeometry object self._target from mgzwarp.metadata['gcamorph_volgeom_trg']
-        self._target = sf.transform.geometry.volgeom_dict2image_geometry(mgzwarp.metadata['gcamorph_volgeom_trg'])
-
-        # not sure if these two are necessary
-        self._spacing   = mgzwarp.metadata['gcamorph_spacing']
-        self._exp_k     = mgzwarp.metadata['gcamorph_exp_k']
+        format = warp.metadata['warpfield_dtfmt']
+        source = transform.volgeom_dict2image_geometry(warp.metadata['gcamorph_volgeom_src'])
+        target = transform.volgeom_dict2image_geometry(warp.metadata['gcamorph_volgeom_trg'])
+        return super().__class__(warp.data, source, target, format, metadata=warp.metadata)
 
 
     #
     # output _data as mgz warp
-    def save(self, filename):
+    def save(self, filename, fmt=None):
         """
         Output _data as mgz warp volume
 
@@ -129,22 +122,13 @@ class Warp:
         ----------
         filename : string
             output mgz warp file
+        fmt : str
+            Optional file format to force.
         """
-         
-        # create a volume from _data
-        mgzwarp = sf.image.framed.cast_image(self._data, fallback_geom=self._target)
-        
-        # set metadata
-        mgzwarp.metadata['intent'] = sf.core.framed.FramedArrayIntents.warpmap
-        mgzwarp.metadata['gcamorph_volgeom_src'] = sf.transform.geometry.image_geometry2volgeom_dict(self._source)
-        mgzwarp.metadata['gcamorph_volgeom_trg'] = sf.transform.geometry.image_geometry2volgeom_dict(self._target)
-        
-        mgzwarp.metadata['warpfield_dtfmt']  = self._format
-        mgzwarp.metadata['gcamorph_spacing'] = self._spacing
-        mgzwarp.metadata['gcamorph_exp_k']   = self._exp_k
-
-        # output the volume as mgz warp
-        mgzwarp.save(filename, None, sf.core.framed.FramedArrayIntents.warpmap)
+        self.metadata['warpfield_dtfmt'] = self.format
+        self.metadata['gcamorph_volgeom_src'] = transform.image_geometry2volgeom_dict(self.source)
+        self.metadata['gcamorph_volgeom_trg'] = transform.image_geometry2volgeom_dict(self.target)
+        super().save(filename, fmt=fmt, intent=sf.core.framed.FramedArrayIntents.warpmap)
 
 
     #
@@ -169,9 +153,9 @@ class Warp:
             return self._data
 
         # cast vox2world.matrix and world2vox.matrix to float32
-        src_vox2ras = self._source.vox2world.matrix.astype('float32')
-        src_ras2vox = self._source.world2vox.matrix.astype('float32')
-        trg_vox2ras = self._target.vox2world.matrix.astype('float32')
+        src_vox2ras = self.source.vox2world.matrix.astype('float32')
+        src_ras2vox = self.source.world2vox.matrix.astype('float32')
+        trg_vox2ras = self.target.vox2world.matrix.astype('float32')
 
         # reshape self._data to (3, n) array, n = c * s * r
         transform = self._data.astype('float32')
@@ -286,64 +270,48 @@ class Warp:
             raise ValueError(f'deformation ({self._data.shape[-1]}D) does not match '
                              f'dimensionality of image ({image.basedim}D)')
 
-        """
-        # get the image in the space of the deformation
-        #source_data = image.resample_like(self._target).framed_data
-        """
-        source_data = image.framed_data
 
         # convert deformation field to disp_crs
         deformationfield = self.convert(self.Format.disp_crs)
 
         # do the interpolation, the function assumes disp_crs deformation field
-        interpolated = interpolate(source=source_data,
-                                   target_shape=self._target.shape,
+        interpolated = interpolate(source=image.framed_data,
+                                   target_shape=self.geom.shape,
                                    method=method,
                                    disp=deformationfield,
                                    fill=fill)
-        
-        deformed = image.new(interpolated, self._target)
-        
-        return deformed
-        
 
-    #
-    # _data getter and setter
-    @property
-    def data(self):
-        return self._data
-    @data.setter
-    def data(self, warp):
-        self._data = warp
+        return self.new(interpolated)
 
 
     #
     # _format getter and setter
     @property
-    def deformationformat(self):
+    def format(self):
         return self._format
-    @deformationformat.setter
-    def deformationformat(self, format):
+
+    @format.setter
+    def format(self, format):
         self._format = format
 
-
-    #
-    # _source getter and setter
     @property
     def source(self):
+        """
+        Source (or moving) image geometry.
+        """
         return self._source
+
     @source.setter
-    def source(self, geom):
-        self._source = geom
+    def source(self, value):
+        self._source = transform.cast_image_geometry(value, copy=True)
 
-
-    #
-    # _target getter and setter
     @property
     def target(self):
-        return self._target
+        """
+        Target (or fixed) image geometry.
+        """
+        return self.geom
+
     @target.setter
-    def target(self, geom):
-        self._target = geom
-
-
+    def target(self, value):
+        self.geom = value
