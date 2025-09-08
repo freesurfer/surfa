3c6ac22a79d8cba9b7348bf4109c8470be37b790
ahoopes <hoopes@mit.edu>
2025-09-08 10:46:33 -0400

    more mesh casting

--- surfa/mesh/__init__.py
+++ surfa/mesh/__init__.py
@@ -1,4 +1,6 @@
 from .mesh import Mesh
+from .mesh import cast_mesh
+from .mesh import is_mesh_castable
 from .overlay import Overlay
 from .overlay import cast_overlay
 from .distance import surface_distance
